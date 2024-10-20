98cf6219953ac26e2168a2f0cf59cdbef1fae4af
SofiDev <angiemeow89@gmail.com>
2024-10-20 01:33:13 -0600

    🚧 Clase35
    \

--- src/components/utils/prevPage.js
+++ src/components/utils/prevPage.js
@@ -1,6 +1,7 @@
 export const prevPage = (id) => {
   document.getElementById(id).addEventListener("click", function () {
-    window.history.back();
+    window.history.back() || window.history.go(-1);
+
 
   });
 };
